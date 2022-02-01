4854a21b222150e33facabb14ad886fe9442af65
OlehShyb <85501468+OlehShyb@users.noreply.github.com>
2022-02-01 17:29:39 +0200

    Server logic

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Server.DataManagers;
+using System;
+using System.IO.MemoryMappedFiles;
+using System.Threading;
+
+namespace Server
+{
+    class Program
+    {
+        static DateTime timeWeather = DateTime.Now, timeRates = DateTime.Now, timeStockePrice = DateTime.Now;
+        static  MemoryMappedFile memoryMappedFileWeatherForecast = MemoryMappedFile.CreateNew("MMF_WeatherForecast", 10000);
+        static MemoryMappedFile memoryMappedFileExchangeRate = MemoryMappedFile.CreateNew("MMF_ExchangeRate", 10000);
+        static MemoryMappedFile memoryMappedFileStockPrices = MemoryMappedFile.CreateNew("MMF_StockPrices", 20000);
+        static Semaphore _pool;
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                if(Math.Abs(timeRates.Day - DateTime.Now.Day) == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"{DateTime.Now} Rate checker is working...");
+                    timeRates = timeRates.AddDays(1);
+                    Rateschecker.CheckRates(memoryMappedFileExchangeRate);
+                }
+                if (Math.Abs(timeWeather.Hour - DateTime.Now.Hour) == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"{DateTime.Now} Weather checker is working...");
+                    timeWeather = timeWeather.AddHours(1);
+                    WeatherChecker.CheckWeather(memoryMappedFileWeatherForecast);
+                }
+                if (Math.Abs(timeStockePrice.Minute - DateTime.Now.Minute) == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine($"{DateTime.Now} Stocke price checker is working...");
+                    timeStockePrice = timeStockePrice.AddMinutes(1);
+                    StockPricesChecker.CheckStockPrices(memoryMappedFileStockPrices);
+                    var isLocked = Semaphore.TryOpenExisting("SynchronizationSemaphore", out _pool);
+                    if (isLocked)
+                        _pool.Release();
+                }
+            }
+        }
+    }
+}
