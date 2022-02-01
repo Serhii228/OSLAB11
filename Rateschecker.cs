f99b335ffae3bf75fcb11248c2cf5144839db2ad
Serhii228 <83615481+Serhii228@users.noreply.github.com>
2022-02-01 17:16:08 +0200

    Add files via upload
    Managers for get data from web

---
+++ Rateschecker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+using Server.Models;
+
+namespace Server.DataManagers
+{
+    public class Rateschecker
+    {
+        public static void CheckRates(MemoryMappedFile memoryMappedFile)
+        {
+            var webRequest = WebRequest.Create("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json") as HttpWebRequest;
+            if (webRequest == null)
+            {
+                return;
+            }
+            webRequest.ContentType = "application/json";
+            webRequest.UserAgent = "Nothing";
+            string toFile = "";
+            using (var s = webRequest.GetResponse().GetResponseStream())
+            {
+                using (var sr = new StreamReader(s))
+                {
+                    var contributorsAsJson = sr.ReadToEnd();
+                    var val = JsonConvert.DeserializeObject<List<Rates>>(contributorsAsJson);
+                    foreach (var item in val)
+                    {
+                        toFile += item + "\n"; 
+                    }
+                }
+            }
+            byte[] data = Encoding.UTF8.GetBytes(toFile);
+            using (MemoryMappedViewAccessor accessor = memoryMappedFile.CreateViewAccessor())
+            {
+                accessor.WriteArray(0, toFile.ToCharArray(), 0, data.Length);
+            }
+            Console.WriteLine(toFile);
+        }
+    }
+}
---
+++ WeatherChecker.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using Server.Models;
+using System;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Net;
+using System.Text;
+
+namespace Server.DataManagers
+{
+    public class WeatherChecker
+    {
+        public static void  CheckWeather(MemoryMappedFile memoryMappedFile)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("http://api.openweathermap.org/data/2.5/weather?q=Lviv&units=metric&appid=74372e553fdda3a188f09f0ec7b2bd72");
+                HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                string respons;
+                using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+                {
+                    respons = streamReader.ReadToEnd();
+                }
+                WeatherResponse weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(respons);
+                string toFile = $"Weather forecast for \"{weatherResponse.Name}\":\n" +
+                        $"Temperature : \n\treal : {weatherResponse.Main.Temp} " +
+                        $"\n\tmin : {weatherResponse.Main.Temp_min} " +
+                        $"\n\tmax : {weatherResponse.Main.Temp_max} " +
+                        $"\nPressure : {weatherResponse.Main.Pressure}" +
+                        $"\nHumidity : {weatherResponse.Main.Humidity}" +
+                        $"\nVisibility : {weatherResponse.Visibility}" +
+                        $"\nWind:" +
+                        $"\n\tspeed : {weatherResponse.Wind.Speed}" +
+                        $"\n\tdeg : {weatherResponse.Wind.Deg}" +
+                        $"\n\tgust : {weatherResponse.Wind.Gust}";
+                byte[] data = Encoding.UTF8.GetBytes(toFile);
+                using (MemoryMappedViewAccessor accessor = memoryMappedFile.CreateViewAccessor())
+                {
+                    accessor.WriteArray(0, toFile.ToCharArray(), 0, data.Length);
+                }
+                Console.WriteLine(toFile);
+            }
+        }
+    }
+}
